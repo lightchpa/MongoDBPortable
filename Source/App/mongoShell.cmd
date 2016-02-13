d34b19c02716c3ac8003ac9518c32069f1f1a1c4
lightchpa <lightchpa@users.noreply.github.com>
2016-02-13 20:53:40 +0100

    Bug fixes and end of line

--- Source/App/mongoShell.cmd
+++ Source/App/mongoShell.cmd
@@ -1,16 +1,18 @@
 @echo off
-
 title MongoDB Command Prompt
 cls
-
-echo Set mongo.exe environment variables in oder to
-echo store .mongorc.js and .dbshell in the Data folder
-echo 
 set HOME=%1
-set HOMEDRIVE=%1
-set HOMEPATH=%1
-echo 
-cd %2
+set HOMEDRIVE=%2
+set HOMEPATH=%3
+cd %1
+echo mongorc.js and .dbshell files are stored in
+echo %1
+IF EXIST %1 (
+echo .
+) ELSE (
+mkdir %1
+echo .
+)
 echo You can now run MongoDB binaries
-echo 
-pause
+echo .
+cmd /k
