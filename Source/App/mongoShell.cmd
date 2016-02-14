21040cada0e2c71412faab621ceda583d381cb0a
lightchpa <lightchpa@users.noreply.github.com>
2016-02-14 19:39:01 +0100

    Set USERPROFILE variable in mongoShell.cmd plus text updates

--- Source/App/mongoShell.cmd
+++ Source/App/mongoShell.cmd
@@ -1,11 +1,12 @@
 @echo off
 title MongoDB Command Prompt
 cls
+set USERPROFILE=%1
 set HOME=%1
 set HOMEDRIVE=%2
 set HOMEPATH=%3
 cd %1
-echo mongorc.js and .dbshell files are stored in
+echo .mongorc.js and .dbshell files are stored in
 echo %1
 echo .
 echo You can now run MongoDB binaries
