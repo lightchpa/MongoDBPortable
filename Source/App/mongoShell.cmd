d481b546b01bf3d23eebfb545ef0a9b998a4148a
lightchpa <lightchpa@users.noreply.github.com>
2016-08-11 17:49:43 +0200

    Updated batch file

--- Source/App/mongoShell.cmd
+++ Source/App/mongoShell.cmd
@@ -1,10 +1,10 @@
 @echo off
 title MongoDB Command Prompt
 cls
-cd %1
 echo .mongorc.js and .dbshell files are stored in
-echo %1
-echo .
+echo %HOME%
+echo.
 echo You can now run MongoDB binaries
-echo .
+echo.
+cd %HOME%
 cmd /k
